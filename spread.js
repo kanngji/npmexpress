3d7599f26439b50094722f1d92dcb3d48fb901eb
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-05 11:02:25 +0900

    복습완료

---
+++ spread.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+// console.log('__dirname', __dirname);
+// console.log('__filename', __filename);
+
+// function spread(first, ...rest) {
+//   console.log(first);
+//   console.log(rest);
+// }
+
+// spread(1, 2, 3, 4, 5, 6, 7);
+
+// const [first, ...rest] = [1, 2, 3, 4, 5];
+
+// console.log(first);
+// console.log(rest);
+
+// const kangjiData = {
+//   name: '강지훈',
+//   gender: 'M',
+//   nickName: 'kangji',
+//   email: 'kangji123@naver.com',
+// };
+
+// const { name, ...kangjiInfo } = kangjiData;
+// console.log(name, kangjiInfo);
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = ['6', '7', '8'];
+
+// const merge = [...arr1, ...arr2];
+// console.log(merge);
+
+// const kangjiData = {
+//   name: '강지훈',
+//   gender: 'M',
+// };
+
+// const kangjiInfo = {
+//   nickName: 'kangkangji',
+//   main: 'kang123@naver.com',
+// };
+
+// const kangji = {
+//   ...kangjiData,
+//   ...kangjiInfo,
+// };
+
+// console.log(kangji);
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr);
+// console.log(...arr);
